03cce6953a7bae7b9c49c7ad8c7feb575beffe8c
caotv1512 <caomanhct@gmail.com>
2024-02-16 15:18:59 +0700

    Feat: Update config db.

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,7 +8,7 @@ const SnakeNamingStrategy =
   require('typeorm-naming-strategies').SnakeNamingStrategy;
 const config: MysqlConnectionOptions = {
   host: process.env.DB_HOST || 'localhost',
-  port: 3306,
+  port: +process.env.DB_PORT ||3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'password',
   type: 'mysql',
