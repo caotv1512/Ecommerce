93e9347236dbb0231668c30550041f70b926094d
caotv1512 <caomanhct@gmail.com>
2024-02-23 19:52:56 +0700

    Feat: Add code upload file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/product/product.module';
 import { UserModule } from './modules/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CloudinaryModule } from './modules/cloudinary/cloudinary.module';
+import { MailModule } from './modules/mail/mail.module';
 import config from 'ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), ProductModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    ProductModule,
+    UserModule,
+    CloudinaryModule,
+    MailModule,
+  ],
   controllers: [],
   providers: [],
 })
